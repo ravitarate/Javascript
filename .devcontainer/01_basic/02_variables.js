34f7ab255c45a7713bb68d226a83c7eed2980192
Ravi Tarate <123928771+ravitarate@users.noreply.github.com>
2025-12-17 04:31:06 +0000

    all chnage

---
+++ .devcontainer/01_basic/02_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14554
+let  accountemail = "ravitarate9009@gmail.com"
+var accountpasswd = "123456"
+accountcity = "pune"
+
+console.log([accountId,accountemail,accountpasswd])
+
+accountemail = "asdc@gmail.com"
+accountpasswd = "789"
+
+console.table([accountemail,accountpasswd])
