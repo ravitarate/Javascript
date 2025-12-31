a8b95b8f58f59fb312f1dd1f6a2df23fb1f42447
Ravi Tarate <123928771+ravitarate@users.noreply.github.com>
2025-12-31 05:28:35 +0000

    done

---
+++ .devcontainer/05_Datatypeinjavascript.js
@@ -0,0 +1,23 @@
+//primitive datatype
+ 
+const score = 100
+const scorevalue = 100.3
+
+const isloggedIn = false
+const outsidetemp = null
+
+const id  = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid)
+
+const heros = ["shaktiman ", "naagraj"]
+
+let myobj = {
+    name : "ravi",
+    age :22,
+}
+
+ const myFunction = function(){
+    console.log("hello world")
+ }
