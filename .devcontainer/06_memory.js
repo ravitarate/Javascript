e773645118b0b1525d4b6fafec007db1d36a8fd4
Ravi Tarate <123928771+ravitarate@users.noreply.github.com>
2026-01-03 06:34:20 +0000

    done

---
+++ .devcontainer/06_memory.js
@@ -0,0 +1,15 @@
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let usertwo ={
+    email :"ravitarate@gmail.com",
+    upi: "user@ybl"
+}
+
+ let userTwo = userone
+ userTwo.email = "ravitarate9009@gmail.com";
+
+console.log(userone.email);
+console.log(usertwo.email);
---
+++ .devcontainer/07_string.js
@@ -0,0 +1,20 @@
+// creating a string 
+let name = "Ravi"
+let msg = "Hello";
+let msg1  = ` hello ravi`
+
+const gamename = new String(`ravi`)
+console.log(gamename[0])
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+console.log(gamename.indexOf('i'))
+
+ const newString = gamename.substring(0,2)
+ console.log(newString)
+
+ const anotherstring = gamename.slice(-8,3)
+ console.log(anotherstring)
+
+ const newStringOne = " Hitesh "
+ console.log(newStringOne)
